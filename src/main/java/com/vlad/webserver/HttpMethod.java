4370bc4c583090410c99d5caeabb921ca8954bb9
Vladislav Degtiar <flatdeh@gmail.com>
2018-06-26 03:19:03 +0300

    WebServer badRequest&Exceptions

--- src/main/java/com/vlad/webserver/HttpMethod.java
+++ src/main/java/com/vlad/webserver/HttpMethod.java
@@ -1,20 +1,20 @@
 package com.vlad.webserver;
 
 public enum HttpMethod {
-  GET("GET");
+    GET("GET");
 
-  private String httpMethodName;
+    private String httpMethodName;
 
-  HttpMethod(String httpMethodName) {
-    this.httpMethodName = httpMethodName;
-  }
-
-  static HttpMethod getHttpMethodByName(String name) {
-    for (HttpMethod httpMethod : HttpMethod.values()) {
-      if (httpMethod.httpMethodName.equalsIgnoreCase(name)) {
-        return httpMethod;
-      }
+    HttpMethod(String httpMethodName) {
+        this.httpMethodName = httpMethodName;
     }
-    throw new IllegalArgumentException("HttpMethodName with name \"" + name + "\" not found");
-  }
+
+    static HttpMethod getHttpMethodByName(String name) {
+        for (HttpMethod httpMethod : HttpMethod.values()) {
+            if (httpMethod.httpMethodName.equalsIgnoreCase(name)) {
+                return httpMethod;
+            }
+        }
+        throw new IllegalArgumentException("HttpMethodName with name \"" + name + "\" not found");
+    }
 }
