08ab601b3ba6507e023c825f6acd671e7a1167ce
Vladislav Degtiar <flatdeh@gmail.com>
2018-06-30 23:25:54 +0300

    ResourceReader

--- src/main/java/com/vlad/webserver/ResponseWriter.java
+++ src/main/java/com/vlad/webserver/ResponseWriter.java
@@ -3,11 +3,11 @@ package com.vlad.webserver;
 import java.io.*;
 
 public class ResponseWriter {
-    private static final int BYTES_BUFFER_SIZE = 256;
-    private static final byte[] NEW_LINE = "\r\n".getBytes();
-    private static final byte[] SUCCESS_RESPONSE = "HTTP/1.1 200 OK\n".getBytes();
-    private static final byte[] NOT_FOUND_RESPONSE = "HTTP/1.1 404 NotFound\n".getBytes();
-    private static final byte[] BAD_REQUEST_RESPONSE = "HTTP/1.1 400 BadRequest\n".getBytes();
+    private static final int BYTES_BUFFER_SIZE =        256;
+    private static final byte[] NEW_LINE =              "\r\n".getBytes();
+    private static final byte[] SUCCESS_RESPONSE =      "HTTP/1.1 200 OK\n".getBytes();
+    private static final byte[] NOT_FOUND_RESPONSE =    "HTTP/1.1 404 NotFound\n".getBytes();
+    private static final byte[] BAD_REQUEST_RESPONSE =  "HTTP/1.1 400 BadRequest\n".getBytes();
     private BufferedOutputStream bufferedOutputStream;
 
 
--- src/main/java/com/vlad/webserver/WebServer.java
+++ src/main/java/com/vlad/webserver/WebServer.java
@@ -10,12 +10,12 @@ public class WebServer {
 
     public void start() throws IOException {
         try (ServerSocket server = new ServerSocket(port)) {
+            ResourceReader resourceReader = new ResourceReader(webAppPath);
             while (true) {
                 try (Socket socket = server.accept();
                      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                      BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(socket.getOutputStream())) {
 
-                    ResourceReader resourceReader = new ResourceReader(webAppPath);
                     RequestHandler requestHandler = new RequestHandler(bufferedReader, bufferedOutputStream, resourceReader);
                     requestHandler.handle();
                 }
